937f61c9035780c1ef4e7905739270f67a058ea7
andradebru <brunansandrade@gmail.com>
2022-09-19 15:30:23 -0300

    imprime no console se o nome é valido a partir do numero de caracteres

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const element = document.getElementById("myBtn");
-element.addEventListener("click", imprimirSalvar);
+element.addEventListener("click", validateForm, imprimirSalvar);
 
 function imprimirSalvar() {
   // console.log("Botão salvar clicado");
   console.log("nome = ", document.getElementById("name").value);
   console.log("data = ", document.getElementById("birth-date").value);
+}
+
+function validateForm() {
+  let nome = document.formCadastro.name.value.length;
+  if (nome > 3 && nome < 120) {
+    console.log("nome valido");
+  } else {
+    console.log("nome invalido");
+  }
+}
+
+function allLetter(inputtxt) {
+  var letters = /^[A-Za-z]+$/;
+  if (inputtxt.value.match(letters)) {
+    return true;
+  } else {
+    alert("message");
+    return false;
+  }
 }
